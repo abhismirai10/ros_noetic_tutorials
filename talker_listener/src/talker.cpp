ba29b36dabc94420fc18682a5c345ad33f54e7ca
Abhi Chothani <abhismirai10@gmail.com>
2024-07-04 03:55:52 -0400

    added proper comments

--- talker_listener/src/talker.cpp
+++ talker_listener/src/talker.cpp
@@ -2,22 +2,22 @@
 #include "std_msgs/String.h"
 #include <iostream>
 
-int main(int argc, char **argv){
-    ros::init(argc, argv, "talker");
+int main(int argc, char **argv){ //see listener node
+    ros::init(argc, argv, "talker"); //init node
     ros::NodeHandle n;
 
-    ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+    ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000); //publication init
     ros::Rate loop_rate(10);
 
     int count = 0;
-    while(ros::ok()){
+    while(ros::ok()){ //ctrl+c stops this
         std_msgs::String msg;
         std::stringstream ss;
         ss<< "hello world "<< count;
         msg.data = ss.str();
 
-        ROS_INFO("%s", msg.data.c_str());
-        chatter_pub.publish(msg);
+        ROS_INFO("%s", msg.data.c_str());//printing or logging the data by publisher node
+        chatter_pub.publish(msg);// publishing
         ros::spinOnce();
         loop_rate.sleep();
         count++;
